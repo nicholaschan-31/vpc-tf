fc6a15945bc0bdfe31a0562d1191c411ec1d1fe2
nicholas33 <nicholas@DEV-c223ff00.localdomain>
2026-03-01 16:37:46 +0800

    add dns support and dns hostname

--- mains.tf
+++ mains.tf
@@ -3,6 +3,9 @@ resource "aws_vpc" "main" {
   cidr_block       = var.cidr
   instance_tenancy = "default"
 
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "${var.name}-vpc"
   }
